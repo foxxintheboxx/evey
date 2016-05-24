daf451aecc8ff9d2eefed95986ffac5316e776e7
Ian Fox <ianfox41@gmail.com>
2016-05-23 21:31:38 -0700

    changed urls in const.py

--- const.py
+++ const.py
@@ -1,7 +1,8 @@
 #IMG_ENDPOINTS
-EXAMPLE_0 = "https://raw.githubusercontent.com/foxxintheboxx/evey/master/app/static/onboarding_imgs/ee143.png?token=AE7-ylvIqLEZiA3FiGEzEz_uQBbbs5VWks5XTQvuwA%3D%3D"
-EXAMPLE_1 = "https://raw.githubusercontent.com/foxxintheboxx/evey/master/app/static/onboarding_imgs/got.png?token=AE7-yqB3eovWRUCS1YL2F4C5VVhn6TYfks5XS9bpwA%3D%3D"
-EXAMPLE_2 = "https://raw.githubusercontent.com/foxxintheboxx/evey/master/app/static/onboarding_imgs/econ100.png?token=AE7-ytr0TOB50ZthzYcnBCyaVps0fVJVks5XS9ycwA%3D%3D"
+
+EXAMPLE_0 = "https://raw.githubusercontent.com/foxxintheboxx/evey/master/app/static/onboarding_imgs/salsa.png?token=AE7-yjQHkXDtLhp_JYvc-5ceQCdM5lK0ks5XTRKmwA%3D%3D"
+EXAMPLE_1 = "https://raw.githubusercontent.com/foxxintheboxx/evey/master/app/static/onboarding_imgs/ee143.png?token=AE7-ylvIqLEZiA3FiGEzEz_uQBbbs5VWks5XTQvuwA%3D%3D"
+EXAMPLE_2 = "https://raw.githubusercontent.com/foxxintheboxx/evey/master/app/static/onboarding_imgs/fam.png?token=AE7-ytKht47y3ljQQgZZ6i4xwVMHzWNWks5XTRNVwA%3D%3D"
 
 ABOUT_0 = "https://www.google.com/logos/2012/haring-12-hp.png"
 ABOUT_1 = "https://s-media-cache-ak0.pinimg.com/736x/40/d4/7b/40d47b9ce63fae42b1ed196b11481bf6.jpg"
