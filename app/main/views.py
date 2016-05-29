bc8976a26207d9fac0bb6ac7ddf3693f44f0c566
Ian Fox <ianfox41@gmail.com>
2016-05-29 02:19:28 -0700

    removed views from main and initiated auth blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -80,29 +80,29 @@ def webhook():
 def load_user(id):
   return User.query.get(int(id))
 
-@main.route('/authorize/facebook')
-def oauth_authorize():
-  if not current_user.is_anonymous:
-    return redirect(url_for('main.index'))
-  oauth = OAuthSignIn.get_provider('facebook')
-  return oauth.authorize()
+# @main.route('/authorize/facebook')
+# def oauth_authorize():
+#   if not current_user.is_anonymous:
+#     return redirect(url_for('main.index'))
+#   oauth = OAuthSignIn.get_provider('facebook')
+#   return oauth.authorize()
 
-@main.route('/callback/facebook')
-def oauth_callback():
-  if not current_user.is_anonymous:
-    return redirect(url_for('main.index'))
-  oauth = OAuthSignIn.get_provider("facebook")
-  user_data = oauth.callback()
-  if user_data.get("fb_uid") is None:
-    return "access Denied"
-  user = usr_manager.handle_fb_user(user_data)
-  if user != None:
-    messenger_uid = user.messenger_uid
-    resp_msg = EveyEngine(user.first_name, user).understand(["site visit"])
-    for msg in resp_msg:
-      payload = {'recipient': {'id': messenger_uid}, 'message':msg}
-      r = requests.post(MESNGR_API_URL + TOKEN, json=payload)
-  return render_template("index.html")
+# @main.route('/callback/facebook')
+# def oauth_callback():
+#   if not current_user.is_anonymous:
+#     return redirect(url_for('main.index'))
+#   oauth = OAuthSignIn.get_provider("facebook")
+#   user_data = oauth.callback()
+#   if user_data.get("fb_uid") is None:
+#     return "access Denied"
+#   user = usr_manager.handle_fb_user(user_data)
+#   if user != None:
+#     messenger_uid = user.messenger_uid
+#     resp_msg = EveyEngine(user.first_name, user).understand(["site visit"])
+#     for msg in resp_msg:
+#       payload = {'recipient': {'id': messenger_uid}, 'message':msg}
+#       r = requests.post(MESNGR_API_URL + TOKEN, json=payload)
+#   return render_template("index.html")
 
 
 @main.route('/')
@@ -110,37 +110,37 @@ def oauth_callback():
 def index():
     return render_template("index.html")
 
-@main.route('/register/<messenger_uid>' , methods=['GET','POST'])
-def register(messenger_uid):
-    print(messenger_uid)
-    if request.method == 'GET':
-        return render_template('register.html')
-    user = User(username=request.form['username'] ,
-                password=request.form['password'],
-                messenger_uid="909043528309")
-    print(user)
-    db.session.add(user)
-    db.session.commit()
-    print('User successfully registered')
-    return redirect(url_for('main.login'))
+# @main.route('/register/<messenger_uid>' , methods=['GET','POST'])
+# def register(messenger_uid):
+#     print(messenger_uid)
+#     if request.method == 'GET':
+#         return render_template('register.html')
+#     user = User(username=request.form['username'] ,
+#                 password=request.form['password'],
+#                 messenger_uid=messenger_uid)
+#     print(user)
+#     db.session.add(user)
+#     db.session.commit()
+#     print('User successfully registered')
+#     return redirect(url_for('main.login'))
 
-@main.route('/login',methods=['GET','POST'])
-def login():
-    if request.method == 'GET':
-        print('hello')
-        x =  render_template('login.html')
-        return x
-    print(request.form)
-    username = request.form['username']
-    password = request.form['password']
-    registered_user = User.query.filter_by(username=username).first()
-    print(registered_user)
-    if (registered_user is None or
-        not registered_user.verify_password(password)):
-        flash('Username or Password is invalid' , 'error')
-        return redirect(url_for('main.login'))
-    login_user(registered_user, remember = True)
-    flash('Logged in successfully')
-    return redirect(request.args.get('next') or url_for('main.index'))
+# @main.route('/login',methods=['GET','POST'])
+# def login():
+#     if request.method == 'GET':
+#         print('hello')
+#         x =  render_template('login.html')
+#         return x
+#     print(request.form)
+#     username = request.form['username']
+#     password = request.form['password']
+#     registered_user = User.query.filter_by(username=username).first()
+#     print(registered_user)
+#     if (registered_user is None or
+#         not registered_user.verify_password(password)):
+#         flash('Username or Password is invalid' , 'error')
+#         return redirect(url_for('main.login'))
+#     login_user(registered_user, remember = True)
+#     flash('Logged in successfully')
+#     return redirect(request.args.get('next') or url_for('main.index'))
 
 
